3c5c8b9d7fac257b091311971201e76664522105
Yx_Zou <hello.yxhome@gmail.com>
2020-03-01 16:59:09 +0800

    Node.js 与 浏览器 共有的一些环境变量

---
+++ nodeTest/index.js
@@ -0,0 +1,20 @@
+// 测试
+
+// nodeTest 控制台输入 node test.js 执行
+console.log('hello world')
+
+// 与浏览器共同存在的函数 
+// 时间
+console.log(Date) 
+// 数学对象
+console.log(Math)
+// 定时器
+console.log(setTimeout)
+// 定时器
+console.log(setInterval)
+
+// 渲染浏览器的下一帧，node.js 不存在
+console.log(requestAnimationFrame) // is not defined
+
+// node.js 为了弥补 requestAnimationFrame 函数，新增 setImmediate
+console.log(setImmediate)
