4b1c9f552d80157a80fcfcb04a95d1e5f6131cad
Yx_Zou <hello.yxhome@gmail.com>
2020-03-16 00:16:38 +0800

    node.js 中，正确的 error 捕获

--- nodeTest/14-1.js
+++ nodeTest/14-1.js
@@ -1,25 +1,26 @@
 
-try { // 执行成功打印
-    // 调用 interview 方法，传入回调函数
-    interview(function () {
-        console.log('smile');
-    })
-} catch (e) {
-    // 捕获到异常打印
-    console.log('cry', e)
-}
+// 调用 interview 方法，传入回调函数
+interview(function (err, res) {
+    // nodejs 回调中约定第一个参数为 error
+    if (err instanceof Error) {
+        // 错误则抛出
+        return console.log('cry')
+    }
+    // 正确打印
+    console.log(res)
+})
 
 
 // 定义一个 interview 方法
 // callback 回调参数
 function interview(callback) {
     setTimeout(() => {
-        if (Math.random() < 0.1) {
-            // 执行成功，回调并返回参数
-            callback('success')
+        if (Math.random() < 0.5) {
+            // 执行成功，回调并返回参数（多个）
+            callback(null, 'success')
         } else {
-            // 执行失败，抛出异常
-            throw new Error('fail')
+            // 回调异常，只传递一个参数
+            callback(new Error('fail'))
         }
     }, 2000)
 }
