d0926824e92b74198dced4a4948ccee3faef30cd
Yx_Zou <hello.yxhome@gmail.com>
2020-03-23 23:57:06 +0800

    新增 http 网页，改造石头剪刀布游戏

---
+++ nodeTest/20/20-2.js
@@ -0,0 +1,48 @@
+// 导入 http ，给 js 提供 http 服务的能力 
+const http = require('http');
+// 导入 url 模块，将接收的 url 进行切割 (location、端口、参数等)
+/** 协议(protocol)://域名(host):端口(port)/路径名(pathname)?请求参数(query)
+ * 所有参数 Url {
+    protocol: null,
+    slashes: null, 
+    auth: null,    
+    host: null,    
+    port: null,    
+    hostname: null,
+    hash: null,
+    search: null,
+    query: null,
+    pathname: '/',  
+    path: '/',
+    href: '/'
+  }
+  */
+const url = require('url');
+
+//（http://nodejs.cn/api/querystring.html#querystring_querystring_stringify_obj_sep_eq_options）
+const querystring = require('querystring');
+// 导入文件模块，读取电脑上的文件
+const fs = require('fs');
+
+http.createServer(function (request, response) {
+    // 获取切割的参数
+    const parsedUrl = url.parse(request.url)
+    // console.log(parsedUrl)
+    // 拦截浏览器默认的图标请求
+    if (parsedUrl.pathname == '/favion.ico') {
+        response.writeHead(200)
+        response.end()
+        return
+    }
+    console.log(parsedUrl.path)
+    if (parsedUrl.pathname == '/game') {
+        // querystring.parse(str[, sep[, eq[, options]]])
+        const query = querystring.parse(parsedUrl.query)
+        const playerAction = query.action
+        console.log(playerAction)
+    }
+    if (parsedUrl.pathname == '/') {
+        // 返回 html 文件
+        fs.createReadStream(__dirname + '/20-1.html').pipe(response)
+    }
+}).listen(3000)
